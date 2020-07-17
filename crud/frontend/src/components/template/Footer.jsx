9cf388ad605b8baf129b5374bc5759c0f3074b96
TiagoLinharess <tiagolinharessouza@gmail.com>
2020-07-17 16:16:21 -0300

    Projeto Cadastro de usuarios

---
+++ crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Developed by
+            <strong> Tiago Souza</strong>
+        </span>
+    </footer>
